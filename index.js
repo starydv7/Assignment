a41da1b6677988b211c8c0b8a624bf8dce9aa886
Pawan Yadav <pawanstar72926@gmail.com>
2025-06-17 10:07:52 +0530

    q11
    Q11 assignment first assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+Q11
+
+
+var age=100;
+
+function displayAge(){
+    console.log("global context",age);
+}
+
+function changeAge(){
+    age=45;
+    console.log("age inside function",age);
+}
+
+displayAge();
+changeAge();
