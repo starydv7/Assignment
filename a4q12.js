c076c078fe52324b723bece1e7ee3b25699e3f55
Pawan Yadav <pawanstar72926@gmail.com>
2025-06-21 01:39:14 +0530

    q12

---
+++ a4q12.js
@@ -0,0 +1,9 @@
+// Asynchronous Code Execution Example
+
+console.log("Message 1");
+
+setTimeout(() => {
+  console.log("Message 2 after 2 seconds");
+}, 2000); // Delay of 2000 milliseconds = 2 seconds
+
+console.log("Message 3");
